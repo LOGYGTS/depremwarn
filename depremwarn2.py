52438e030575e737bd4018cba846199fcbb316a9
LOGYGTS <143490592+LOGYGTS@users.noreply.github.com>
2025-06-25 21:22:24 +0300

    Update depremwarn2.py

--- depremwarn2.py
+++ depremwarn2.py
@@ -59,4 +59,5 @@ def haritalar():
     return render_template("haritalar.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render uyumlu
+    app.run(host="0.0.0.0", port=port)
